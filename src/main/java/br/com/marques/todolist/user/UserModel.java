505692b2876a456a00b4ab19def5890522c955a0
Marcos Marques <marcosmarques.sw@gmail.com>
2023-10-13 10:46:22 -0400

    fist commit

---
+++ src/main/java/br/com/marques/todolist/user/UserModel.java
@@ -0,0 +1,12 @@
+package br.com.marques.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+
+  public String username;
+  public String name;
+  public String password;
+  
+}
